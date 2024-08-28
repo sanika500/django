7ccc26c4ab730bd6ee9252ad408142a91132b78d
adam-aswin <aswinsachu2118@gmail.com>
2024-08-28 09:00:49 +0530

    done

--- demopro/mainapp/urls.py
+++ demopro/mainapp/urls.py
@@ -5,6 +5,7 @@ urlpatterns=[
     path('',views.index,name="index"),
     path('about',views.about,name="about"),
     path('demo',views.demo,name="demo"),
+    path('hello',views.hello,name="hello"),
 
 
 ]
--- demopro/mainapp/views.py
+++ demopro/mainapp/views.py
@@ -8,4 +8,6 @@ def about(request):
     return render(request,"about.html")
 def demo(request):
     return render(request,"demo.html")
+def hello(request):
+    return render(request,"hello.html")
 
