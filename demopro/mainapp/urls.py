6190bb1775e33eade85b1187ecf899438a467ab3
adam-aswin <aswinsachu2118@gmail.com>
2024-08-30 08:57:35 +0530

    done

--- demopro/mainapp/urls.py
+++ demopro/mainapp/urls.py
@@ -3,6 +3,10 @@ from .import views
 
 urlpatterns=[
     path('',views.index,name="index"),
+    path('delete/<int:pk>',views.delete_g,name="delete"),
+    path('edit/<int:pk>',views.edit_g,name="edit")
+
+
     # path('about',views.about,name="about"),
     # path('demo',views.demo,name="demo"),
     # path('hello',views.hello,name="hello"),
--- demopro/mainapp/views.py
+++ demopro/mainapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import*
 #     return render(request,"index.html")
 # def about(request):
 #     return render(request,"about.html")
@@ -13,6 +13,21 @@ def index (request):
         name=request.POST['name']
         age=request.POST['age']
         mark=request.POST['mark']
-        return render(request,'index.html')
+        obj=details(name=name,age=age,mark=mark)
+        obj.save()
+        return redirect(index)
+    obj=details.objects.all()
+    return render(request,'index.html',{'obj':obj})
+def delete_g (request,pk):
+    prodobj=details.objects.get(pk=pk)
+    prodobj.delete()
+    return redirect("index")
+
+def edit_g(request,pk):
+    prodobj=details.objects.get(pk=pk)
+    prodobj.edit()
+    return render(request,"hello.html")
+
+
 
 
